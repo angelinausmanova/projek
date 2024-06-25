6ee7bab55df05e66b6dc90724ebd76f7c756b196
angelinausmanova <usmanova-a@mail.ru>
2024-06-25 08:57:14 +0300

    Add files via upload

---
+++ modul.py
@@ -0,0 +1,15 @@
+grades = [[5,3,3,5,4],[2,2,2,3],[4,4,3],[4,5,5,5,2],[5,5,5,4,5]]
+students = {'Johnny','Bilbo','Steve','Khendrik','Aaron'}
+s_ball = {}
+print(list(students))
+ball = [sum(grades[0])/len(grades[0]), sum(grades[1])/len(grades[1]), sum(grades[2])/len(grades[2]),
+            sum(grades[3])/len(grades[3]),sum(grades[4])/len(grades[4])]
+print(ball)
+print(dict(zip(sorted(students),ball)))
+
+
+
+
+
+
+
